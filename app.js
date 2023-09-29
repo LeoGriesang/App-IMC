39c09673f0af6c137221a7e30168e0c63a6eef58
Leonardo Griesang <griesang.leonardo@outlook.com>
2023-09-29 11:05:14 -0300

    responsivo

--- app.js
+++ app.js
@@ -5,59 +5,60 @@ const div = document.querySelector('.card');
 const inputIdade = document.querySelector('#idade');
 const inputAltura = document.querySelector('#altura');
 const inputPeso = document.querySelector('#peso');
-const listaI = document.querySelectorAll('[type="number"]');
 const valorJs = document.querySelector('.valorJS');
 const classificacao = document.querySelector('.classificacao');
 
-//mini validador
-listaI.forEach((item) => {
-    item.addEventListener('change', () => {
-        if (Number.isInteger(+item.value)) {
-            alert('altura e peso precisam estar separados por .')
-        }
-    })
+inputAltura.addEventListener('change', () => {
+    if (Number.isInteger(+inputAltura.value)) {
+        alert('A altura precisa estar separada por .');
+        inputAltura.focus();
+    }
 })
 
 btnCalcular.addEventListener('click', calcularImc);
 
 function calcularImc(event) {
-    let valorAltura = +inputAltura.value;
-    let valorPeso = +inputPeso.value;
+    if (inputAltura.value != '' && inputPeso.value != '') {
+        let valorAltura = +inputAltura.value;
+        let valorPeso = +inputPeso.value;
 
-    const imc = (valorPeso / (valorAltura * valorAltura)).toFixed(2)
-    valorJs.innerHTML = imc
-    if (imc < 18.5) {
-        //abaixo do peso
-        valorJs.style.color = '#62D7D0';
-        classificacao.innerHTML = 'Abaixo do peso';
-        classificacao.style.color = '#62D7D0';
-    } else if (imc <= 24.9) {
-        //peso normal
-        valorJs.style.color = '#6FCF97';
-        classificacao.innerHTML = 'Peso normal';
-        classificacao.style.color = '#6FCF97';
-    } else if (imc <= 29.9) {
-        //pré-obesidade
-        valorJs.style.color = '#F8C648';
-        classificacao.innerHTML = 'Pré-obesidade';
-        classificacao.style.color = '#F8C648';
-    } else if (imc <= 34.9) {
-        //Obesidade grau 1
-        valorJs.style.color = '#F89C48';
-        classificacao.innerHTML = 'Obesidade Grau 1';
-        classificacao.style.color = '#F89C48';
-    } else if (imc <= 39.9) {
-        //Obesidade grau 2
-        valorJs.style.color = '#F87D48';
-        classificacao.innerHTML = 'Obesidade Grau 2';
-        classificacao.style.color = '#F87D48';
+        const imc = (valorPeso / (valorAltura * valorAltura)).toFixed(2)
+        valorJs.innerHTML = imc
+        if (imc < 18.5) {
+            //abaixo do peso
+            valorJs.style.color = '#62D7D0';
+            classificacao.innerHTML = 'Abaixo do peso';
+            classificacao.style.color = '#62D7D0';
+        } else if (imc <= 24.9) {
+            //peso normal
+            valorJs.style.color = '#6FCF97';
+            classificacao.innerHTML = 'Peso normal';
+            classificacao.style.color = '#6FCF97';
+        } else if (imc <= 29.9) {
+            //pré-obesidade
+            valorJs.style.color = '#F8C648';
+            classificacao.innerHTML = 'Pré-obesidade';
+            classificacao.style.color = '#F8C648';
+        } else if (imc <= 34.9) {
+            //Obesidade grau 1
+            valorJs.style.color = '#F89C48';
+            classificacao.innerHTML = 'Obesidade Grau 1';
+            classificacao.style.color = '#F89C48';
+        } else if (imc <= 39.9) {
+            //Obesidade grau 2
+            valorJs.style.color = '#F87D48';
+            classificacao.innerHTML = 'Obesidade Grau 2';
+            classificacao.style.color = '#F87D48';
+        } else {
+            //Obesidade grau 3
+            valorJs.style.color = '#F85348';
+            classificacao.innerHTML = 'Obesidade Grau 3';
+            classificacao.style.color = '#F85348';
+        }
+        div.classList.add('ativo');
     } else {
-        //Obesidade grau 3
-        valorJs.style.color = '#F85348';
-        classificacao.innerHTML = 'Obesidade Grau 3';
-        classificacao.style.color = '#F85348';
+        alert('Preencha todos os campos')
     }
-    div.classList.add('ativo');
 }
 
 btnLimpar.addEventListener('click', () => {
